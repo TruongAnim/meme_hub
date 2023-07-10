00ae73472799939c2ba57be340c59526aaf04331
TruongAnim <truonganim.dev@gmail.com>
2023-07-10 17:55:48 +0700

    add dotenv

--- server/config/db/index.js
+++ server/config/db/index.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 async function connect() {
   try {
-    // await mongoose.connect("mongodb://127.0.0.1:27017/meme_hub");
-    // admin@123 -> admin%40123
-    await mongoose.connect("mongodb+srv://meme_hub_admin:admin%40123@memedb01.illfamh.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect(process.env.MONGODB_URI);
     
     console.log("Connect to db successfully");
   } catch (e) {
